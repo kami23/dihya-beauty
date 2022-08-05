e7f52e20bc33483fb99ad5a69503dad0591152d8
kami23 <fn_khettache@esi.dz>
2022-08-05 17:26:29 +0100

    numerous ball styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { gsap } from "gsap";
 
 function App() {
+  const [backgroudColor, setBackgroundColor] = useState("red");
+  const [animationComplete, setAnimationComplete] = useState(false);
+  const completeAnimation = () => {
+    setAnimationComplete(true);
+    document.body.style.overflowY = "auto";
+  };
+  // const bounceTransition = {
+  //   y: {
+  //     duration: 0.4,
+  //     yoyo: Infinity,
+  //     ease: "easeOut",
+  //   },
+  //   backgroundColor: {
+  //     duration: 0,
+  //     yoyo: Infinity,
+  //     ease: "easeOut",
+  //     repeatDelay: 0.8,
+  //   },
+  // };
+  useEffect(() => {
+    // Inner Page height for mobile devices
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    // GSAP animation
+    let tl = gsap.timeline();
+    const homeAnimation = (animation) => {
+      tl.to(".ballIntro", {
+        duration: 2,
+        y: "100vh",
+        ease: "bounce.out",
+      })
+        .to(".ballIntro", {
+          duration: 1,
+          scale: 30,
+          ease: "power3.out",
+          onComplete: animation,
+        })
+        .from(".after-animation", {
+          duration: 0.8,
+          opacity: 0,
+          ease: "power3.out",
+        });
+    };
+    homeAnimation(completeAnimation);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AnimatePresence exitBeforeEnter>
+      {animationComplete === false && (
+        <div className="Intro">
+          <motion.div className="ballIntro"></motion.div>
+        </div>
+      )}
+      <div className="after-animation"></div>
+    </AnimatePresence>
   );
 }
 
